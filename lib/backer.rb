f3eacec44f4b97e6e8eed1d13b75d44c3c92a812
Stef Spears <spears.sj87@gmail.com>
2019-10-13 23:14:03 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,9 +2,11 @@ require 'pry'
 
 class Backer 
   
-  attr_reader :backed_projects
+  attr_reader :name, :backed_projects
   
-  def initialize
+  def initialize(name)
+    @name = name 
+    @backed_projects = []
   end
   
   
